fa63911fbc71b663c9553d1e5a649b96c0d9bbda
MohitR1999 <mohitr1999@gmail.com>
2023-02-04 18:35:41 +0530

    Added API request integration for adding node

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,24 +1,42 @@
 function appRibbonOnClickHandler(itemId) {
     if (itemId == "add_node") {
         appNodeAdditionWindow.show();
-    } 
+    }
 }
 
 function appNodeAdditionFormOnButtonClickHandler(name) {
     if (name == "proceed") {
         const ipAddress = appNodeAdditionForm.getItemValue("ip");
+        appNodeAdditionWindow.progressOn();
         if (ipAddress) {
             appNodeAdditionWindowStatusBar.setText("Add a node by specifying the IP address");
             const requestBody = {
-                ip : ipAddress
+                ip: ipAddress
             };
-            console.log(requestBody);
-    
-            // reset the values and close the window
-            appNodeAdditionForm.setItemValue("ip", "");
-            appNodeAdditionWindow.hide();
+            fetch(`${BASE_URL}/addnode`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(requestBody)
+            }).then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    // reset the values and close the window
+                    appNodeAdditionWindow.progressOff();
+                    appNodeAdditionForm.setItemValue("ip", "");
+                    appNodeAdditionWindow.hide();
+                })
+                .catch(err => {
+                    console.log(err);
+                    appNodeAdditionWindow.progressOff();
+                    appNodeAdditionForm.setItemValue("ip", "");
+                    appNodeAdditionWindow.hide();
+                })
+
         } else {
             appNodeAdditionWindowStatusBar.setText("Please provide a valid IP address");
+            appNodeAdditionWindow.progressOff();
         }
     }
 }
